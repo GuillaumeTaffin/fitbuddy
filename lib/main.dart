2eaca6a5a8d41f3dc783bef8952c35ab4814bf96
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-11-17 19:31:57 -0500

    Add Supabase dep to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:fitbuddy/back/utils.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await connectSupabase();
+
   runApp(const FitBuddyApp());
 }
 
